85bb08437ecc3f38cb37e9d08075d6bd2a71551b
YANGJUNHO <yangwork96@gmail.com>
2024-04-11 21:00:55 +0900

    Feat: AddProduct components 수정

--- src/pages/product/AddProduct.jsx
+++ src/pages/product/AddProduct.jsx
@@ -1,5 +1,22 @@
+<<<<<<< HEAD
 function AddProduct() {
   return <div>AddProduct</div>;
+=======
+import Calendar from './addproduct/Calendar';
+import ProductImage from './addproduct/ProductImage';
+import ProductContent from './addproduct/ProductContent';
+import ProductSelectSpot from './addproduct/ProductSelectSpot';
+
+function AddProduct() {
+  return (
+    <div>
+      <ProductImage />
+      <Calendar />
+      <ProductSelectSpot />
+      <ProductContent />
+    </div>
+  );
+>>>>>>> 3e5f52c (Feat: AddProduct components 수정)
 }
 
 export default AddProduct;
