b479d87686f678282bdbc1ff58da2654c6661748
D0-HA <withchemi970@gmail.com>
2024-04-12 00:03:21 +0900

    Docs: 메인, 상세 검색 페이지 마크업
    - 메인페이지 타이틀, 검색창 마크업
    - 상세 검색 페이지 import css

--- src/pages/product/DetailedSearch.jsx
+++ src/pages/product/DetailedSearch.jsx
@@ -1,6 +1,6 @@
- import Header from '@components/layout/Header';
+import Header from '@components/layout/Header';
 import { Outlet } from 'react-router-dom';
-import '@components/style/layout.css';
+import '@components/style/detailedSearch.css';
 
 function DetailedSearch() {
   return (
--- src/pages/product/MainProductList.jsx
+++ src/pages/product/MainProductList.jsx
@@ -1,12 +1,33 @@
 import Footer from '@components/layout/Footer';
 import { Outlet } from 'react-router-dom';
-import '@components/style/layout.css';
+import '@components/style/mainProductList.css';
 
 function MainProductList() {
   return (
     <div className="layout">
       <div className="scrollbar">
+        <div className="titleBox">
+          <img
+            src="../src/assets/icons/icon-plane.svg"
+            className="img-title"
+          ></img>
+          <h1 className="title">Trip with me</h1>
+        </div>
         <Outlet />
+        <div className="searhBox">
+          <input
+            type="search"
+            placeholder="여행지나 키워드를 검색해보세요."
+            className="searchInput"
+          />
+          <button type="button">
+            {' '}
+            <img
+              src="../src/assets/icons/icon-search-bar.svg"
+              className="img-detailSearch"
+            ></img>
+          </button>
+        </div>
       </div>
       <Footer />
     </div>
