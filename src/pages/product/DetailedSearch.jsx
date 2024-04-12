ab32a4ffc7b16c3d864a6b9424328d64c8739c27
D0-HA <withchemi970@gmail.com>
2024-04-12 15:38:53 +0900

    Docs: 상세 검색 페이지 마크업 완료

--- src/pages/product/DetailedSearch.jsx
+++ src/pages/product/DetailedSearch.jsx
@@ -8,6 +8,77 @@ function DetailedSearch() {
       <Header />
       <div className="scrollbar">
         <Outlet />
+        <div className="searchBox">
+          <h2 className="searchBox-title">지역을 선택해주세요</h2>
+
+          <ul className="searchBox-detail">
+            <li>
+              {' '}
+              <input type="checkbox" id="1" />
+              <label htmlFor="1">기능</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="2" />
+              <label htmlFor="2">구현은</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="3" />
+              <label htmlFor="3">언제..</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="4" />
+              <label htmlFor="4">훌쩍</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="5" />
+              <label htmlFor="5">또륵</label>
+            </li>
+          </ul>
+        </div>
+
+        <div className="searchBox">
+          <h2 className="searchBox-title">테마를 선택해주세요</h2>
+
+          <ul className="searchBox-detail">
+            <li>
+              {' '}
+              <input type="checkbox" id="1" />
+              <label htmlFor="1">아니</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="2" />
+              <label htmlFor="2">너무</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="3" />
+              <label htmlFor="3">어렵다</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="4" />
+              <label htmlFor="4">컴포넌트</label>
+            </li>
+
+            <li>
+              {' '}
+              <input type="checkbox" id="5" />
+              <label htmlFor="5">언제 나누지</label>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
