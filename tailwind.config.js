3d47d71a5f108218e9c90d6dc86a902efddd6609
YANGJUNHO <yangwork96@gmail.com>
2024-04-14 21:48:11 +0900

    Docs: tailwind.config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': '#68A9ED',
+        'light-gray': '#EFEFF0',
+        'mid-gray': '#AFB1B6',
+        'warning-color': '#E72929',
+      },
+      fontFamily: {
+        notosans: ['Noto Sans KR'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
