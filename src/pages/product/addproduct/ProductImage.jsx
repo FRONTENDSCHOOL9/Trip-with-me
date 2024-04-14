a5b9198beddf1c408e0a59a131a9c437b73e21a1
YANGJUNHO <yangwork96@gmail.com>
2024-04-14 21:53:02 +0900

    Feat: 프로덕트 이미지 추가 기능

--- src/pages/product/addproduct/ProductImage.jsx
+++ src/pages/product/addproduct/ProductImage.jsx
@@ -5,33 +5,59 @@ function ProductImage() {
   const { setProductInfo } = useProductInfostore();
 
   const [selectedFile, setSelectedFile] = useState(null);
+  const [showUploadPrompt, setShowUploadPrompt] = useState(false);
 
   const handleFileChange = event => {
-    setSelectedFile(URL.createObjectURL(event.target.files[0]));
+    setSelectedFile(URL.createObjectURL(event.target.files[0])); //로컬로 이미지 미리보기
+    setShowUploadPrompt(false);
   };
 
   const onSubmit = event => {
     event.preventDefault();
-    setProductInfo({
-      mainImages: [selectedFile],
-    });
+    if (selectedFile) {
+      setProductInfo({
+        mainImages: [selectedFile],
+      });
+    } else {
+      setShowUploadPrompt(true);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="mainImages">여행 관련 이미지를 추가해주세요</label>
-        <img
-          className="w-72 h-48 bg-slate-200 "
-          src={selectedFile}
-          alt="대표 이미지"
-        />
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={onSubmit}
+      >
+        <p className="text-2xl font-notosans font-semibold text-main-color my-20">
+          여행 관련 이미지를 추가해주세요.
+        </p>
+        <label htmlFor="mainImages" className="relative inline-block">
+          <div className="relative w-80 h-72 bg-light-gray border-mid-gray border-2 rounded-lg">
+            <img
+              className="w-full h-full object-cover rounded-lg"
+              src={selectedFile}
+              alt=""
+            />
+            {!selectedFile && (
+              <div className="absolute inset-0 flex items-center justify-center">
+                <img src="/src/assets/icons/icon-image.png" alt="" />
+              </div>
+            )}
+          </div>
+        </label>
         <input
+          className="hidden"
           type="file"
           accept="image/*"
           id="mainImages"
           onChange={handleFileChange}
         />
+        {showUploadPrompt && (
+          <p className="text-warning-color font-notosans font-medium text-sm">
+            이미지를 업로드해주세요.
+          </p>
+        )}
         <button type="submit">다음</button>
       </form>
     </div>
