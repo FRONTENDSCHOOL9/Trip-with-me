6d671eacf43bfa55962ec631eb837991058d2579
YANGJUNHO <yangwork96@gmail.com>
2024-04-14 21:52:43 +0900

    Feat: 상세정보입력 기능 추가

--- src/pages/product/addproduct/ProductContent.jsx
+++ src/pages/product/addproduct/ProductContent.jsx
@@ -1,28 +1,47 @@
 import { useProductInfostore } from '@zustand/productInfo.mjs';
+import { useState } from 'react';
 
 function ProductContent() {
   const { setProductInfo } = useProductInfostore();
+  const [showUploadContent, setShowUploadPrompt] = useState(false);
 
   const handleSubmit = event => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const content = formData.get('content');
-    setProductInfo({ content: content });
+
+    if (!content.trim()) {
+      setShowUploadPrompt(true);
+    } else {
+      setProductInfo({ content: content });
+    }
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center"
+      >
         <label htmlFor="content">
-          <p>여행 상세 정보를 입력해주세요</p>
-          <p>예시 차량,숙박,제공등</p>
+          <p className="text-2xl font-notosans font-semibold text-main-color mt-20">
+            여행 상세 정보를 입력해주세요
+          </p>
+          <p className="text-sm text-center font-notosans  text-main-color mt-4 mb-10">
+            예시) 차량,숙박,제공등
+          </p>
         </label>
         <textarea
           id="content"
           name="content" // name 속성 추가
-          className="w-72 h-48 border-slate-950  bg-slate-200"
+          className="relative w-80 h-96 bg-light-gray  rounded-lg"
           placeholder="상세 정보를 입력해주세요"
         ></textarea>
+        {showUploadContent && (
+          <p className="text-warning-color font-notosans font-medium text-sm">
+            여행 상세 정보를 입력해주세요.
+          </p>
+        )}
         <button type="submit">다음</button>
       </form>
     </div>
