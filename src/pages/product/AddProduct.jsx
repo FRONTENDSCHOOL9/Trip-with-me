52af1cb9ae055b55af0d127b7878b47b6af1fac0
YANGJUNHO <yangwork96@gmail.com>
2024-04-14 21:51:37 +0900

    Test: 화면프레임 UI, Header컴포넌트 추가 테스트

--- src/pages/product/AddProduct.jsx
+++ src/pages/product/AddProduct.jsx
@@ -2,10 +2,14 @@ import Calendar from './addproduct/Calendar';
 import ProductImage from './addproduct/ProductImage';
 import ProductContent from './addproduct/ProductContent';
 import ProductSelectSpot from './addproduct/ProductSelectSpot';
+import Header from '@components/layout/Header';
+
+import '@components/style/layout.css';
 
 function AddProduct() {
   return (
-    <div>
+    <div className="layout scrollbar">
+      <Header />
       <ProductImage />
       <Calendar />
       <ProductSelectSpot />
