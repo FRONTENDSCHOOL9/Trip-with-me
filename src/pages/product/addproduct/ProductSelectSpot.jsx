9eed72c7c51eb9ef1002f51ad228e300a4c6e5a0
YANGJUNHO <yangwork96@gmail.com>
2024-04-14 21:53:29 +0900

    Feat: 프로덕트 스팟 위치 선택기능 추가

--- src/pages/product/addproduct/ProductSelectSpot.jsx
+++ src/pages/product/addproduct/ProductSelectSpot.jsx
@@ -1,15 +1,27 @@
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import { useProductInfostore } from '@zustand/productInfo.mjs';
 import { useEffect, useState } from 'react';
 
 function ProductSelectSpot() {
   const { productInfo, setProductInfo } = useProductInfostore();
+  const [showUploadPrompt, setShowUploadPrompt] = useState(false);
+
+  const axios = useCustomAxios();
 
   const [tripSpots, setTripSpots] = useState([]);
+
   useEffect(() => {
-    fetch('https://660fa056356b87a55c51d7b9.mockapi.io/api/v1/tripSpot')
-      .then(response => response.json())
-      .then(data => setTripSpots(data));
+    getTripSpot();
   }, []);
+
+  const getTripSpot = async () => {
+    try {
+      const { data } = await axios.get(import.meta.env.VITE_TRAVEL_SPOT_API);
+      setTripSpots(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleCheckboxChange = spot => {
     const selectedSpots = productInfo.selectSpot.slice(); // 현재 선택된 여행 장소 배열 복제
@@ -22,28 +34,43 @@ function ProductSelectSpot() {
       // 이미 선택된 경우 배열에서 제거
       selectedSpots.splice(spotIndex, 1);
     }
-
-    // 상태 업데이트
     setProductInfo({ selectSpot: selectedSpots });
+    // 상태 업데이트
   };
 
   // 폼 제출 핸들러
   const handleSubmit = event => {
-    event.preventDefault(); // 폼 기본 동작 방지
-    // 여기서 추가적인 작업 수행 가능
+    event.preventDefault();
+    if (productInfo.selectSpot.length === 0) {
+      setShowUploadPrompt(true); // 여행 장소를 선택하지 않은 경우 안내 문구를 표시합니다.
+    } else {
+      // 여기서 추가적인 작업 수행 가능
+    }
   };
 
   console.log(productInfo);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <p>여행지를 골라주세요.</p>
-        <ul className="flex flex-wrap">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center"
+      >
+        <p className="text-2xl font-notosans font-semibold text-main-color my-20">
+          여행지를 골라주세요.
+        </p>
+        <ul className=" m-6 flex flex-wrap  gap-x-2 gap-y-4 justify-center items-center font-notosans ">
           {tripSpots.map((spot, id) => (
-            <li className="px-4 py-2" key={id}>
-              <label>
+            <li key={id}>
+              <label
+                className={`flex border-2  rounded-full py-1 px-4 cursor-pointer ${
+                  productInfo.selectSpot.includes(spot)
+                    ? 'border-main-color border-2 '
+                    : 'border-light-gray'
+                }`}
+              >
                 <input
+                  className="hidden"
                   type="checkbox"
                   checked={productInfo.selectSpot.includes(spot)}
                   onChange={() => handleCheckboxChange(spot)}
@@ -57,6 +84,11 @@ function ProductSelectSpot() {
             </li>
           ))}
         </ul>
+        {showUploadPrompt && (
+          <p className="text-warning-color font-notosans font-medium text-sm">
+            여행지를 선택해주세요.
+          </p>
+        )}
         <button type="submit">다음</button>
       </form>
     </div>
