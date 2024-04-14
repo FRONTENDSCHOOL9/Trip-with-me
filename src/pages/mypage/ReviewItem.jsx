b2cb452b9c646b1be1bdb8b4c2aa60258dc4d418
chaeyoungg <chaeyoungg77@gmail.com>
2024-04-14 20:45:31 +0900

    Feat: 후기 작성

---
+++ src/pages/mypage/ReviewItem.jsx
@@ -0,0 +1,53 @@
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+
+// {
+//   "order_id": 1,
+//   "product_id": 2, -> 여행장 userID
+//   "rating": 3,
+//   "content": "친절하셨어요",
+// }
+//시나리오 : 게시물 작성자 ID 와 후기 order_id가 같은 경우에만 해당 후기 불러오기
+
+ReviewItem.propTypes = {
+  item: PropTypes.object,
+};
+
+function ReviewItem({ item }) {
+  console.log('item => ', item);
+  const { register, handleSubmit } = useForm();
+  const axios = useCustomAxios();
+
+  const onSubmit = async formData => {
+    try {
+      const orderId = item._id;
+      const prodId = item.products[0].seller_id;
+      const review = {
+        order_id: orderId,
+        product_id: prodId,
+      };
+      const newReview = {
+        ...review,
+        ...formData,
+      };
+
+      const res = await axios.post('/replies', newReview);
+      console.log('후기 res =>', res);
+      alert('후기 작성 완료하였습니다.');
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register('content')} />
+        <button type="submit">작성하기</button>
+      </form>
+    </div>
+  );
+}
+
+export default ReviewItem;
