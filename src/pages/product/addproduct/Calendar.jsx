8f34ac7645da6a742206fe9c260ba919a18341c7
YANGJUNHO <yangwork96@gmail.com>
2024-04-14 21:52:12 +0900

    Feat: 캘린더기능 추가

--- src/pages/product/addproduct/Calendar.jsx
+++ src/pages/product/addproduct/Calendar.jsx
@@ -6,6 +6,8 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import ko from 'date-fns/locale/ko';
 import { useState } from 'react';
 
+import './productStyle/Calendar.css';
+
 function Calendar() {
   // Zustand에서 상태와 상태 업데이트 함수 가져오기
   const { setProductInfo } = useProductInfostore();
@@ -28,7 +30,13 @@ function Calendar() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center"
+      >
+        <p className="text-2xl font-notosans font-semibold text-main-color my-20">
+          여행 날짜를 선택해주세요.
+        </p>
         <DateRange
           locale={ko}
           editableDateInputs={true}
