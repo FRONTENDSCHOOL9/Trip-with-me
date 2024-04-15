d5a8ed3b2a57ec3e3eedada5aee1e049bfd37739
chaeyoungg <chaeyoungg77@gmail.com>
2024-04-15 23:12:10 +0900

    Design : 예약하기 UI 디자인

--- src/pages/product/Payment.jsx
+++ src/pages/product/Payment.jsx
@@ -1,3 +1,4 @@
+import { Submit } from '@components/style/StyledButton';
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import useMemberState from '@zustand/memberState.mjs';
 import usePageStore from '@zustand/pageName.mjs';
@@ -14,7 +15,7 @@ function Payment() {
   const [productData, setProductData] = useState({});
   const [productCount, setProductCount] = useState(1);
   const [productPrice, setProductPrice] = useState(0);
-  const [productQuantity, setProductQuentity] = useState(0);
+  const [productQuantity, setProductQuantity] = useState(0);
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
 
@@ -34,16 +35,23 @@ function Payment() {
 
   const getProductData = async () => {
     try {
-      const { data } = await axios.get(`/products/${_id}`);
-      setProductData(data);
-      setProductPrice(data?.item?.price);
-      setProductQuentity(data?.item?.buyQuantity);
+      const { data } = await axios.get('/products');
+      const productItem = data?.item?.filter(item => {
+        return item._id == _id;
+      });
+      console.log('productItem', productItem);
+      setProductData(productItem[0]);
+      setProductPrice(productItem[0]?.price);
+      const leftQuantity =
+        +productItem[0]?.quantity - +productItem[0]?.buyQuantity;
+      setProductQuantity(leftQuantity);
     } catch (err) {
       console.log(err.message);
     }
   };
 
   const checkUpCount = () => {
+    console.log(productQuantity);
     if (productCount >= productQuantity) {
       alert('모집 가능 인원을 초과하였습니다.');
       setProductCount(prevCount => prevCount - 1);
@@ -95,31 +103,50 @@ function Payment() {
   return (
     <>
       {user && (
-        <div>
-          <img
-            src={`${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${productData?.item?.mainImages?.name}`}
-          />
-          <p>이미지</p>
-          <p>{productData?.item?.name}</p>
-          <p>테마</p>
+        <div className="p-2">
+          <div className="h-24 bg-gray-200 rounded-md mb-2">
+            <img
+              src={`${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${productData?.item?.mainImages?.name}`}
+            />
+          </div>
+
+          <p className="mb-2">{productData?.name}</p>
+          <p>
+            {productData?.extra?.themes.map(item => (
+              <p
+                className="p-0.5 mx-1 mb-2 border-2 border-main-color rounded-md"
+                key={item.id}
+              >
+                {item.name}
+              </p>
+            ))}
+          </p>
+          <hr className="border-0 bg-gray-100 h-2 my-10" />
+
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <p>인원 선택</p>
-              <button type="button" onClick={handleUp}>
-                △
-              </button>
-              <p>{productCount}</p>
+            <p className="font-bold">인원 선택</p>
+            <div className="flex gap-20 h-10 bg-gray-200 items-center mx-auto justify-center rounded">
               <button type="button" onClick={handleDown}>
                 ▽
               </button>
+              <p>{productCount}</p>
+              <button type="button" onClick={handleUp}>
+                △
+              </button>
             </div>
+            <hr className="border-0 bg-gray-100 h-2 my-10" />
             <div>
-              <p>기타 요청 사항</p>
-              <input type="text" id="request" {...register('address.value')} />
+              <p className="font-bold">기타 요청 사항</p>
+              <input
+                className="h-20 bg-gray-200 w-full rounded-md"
+                type="text"
+                id="request"
+                {...register('address.value')}
+              />
             </div>
-            <button type="submit">
-              {productCount * productPrice}원 결제하기
-            </button>
+            <hr className="border-0 bg-gray-100 h-2 my-10" />
+
+            <Submit>{productCount * productPrice}원 결제하기</Submit>
           </form>
         </div>
       )}
