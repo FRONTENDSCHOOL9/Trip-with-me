860f052636722cb6fd7da3031103a139fe9318c7
YANGJUNHO <yangwork96@gmail.com>
2024-04-15 17:49:06 +0900

    Docs: addProduct 컴포넌트 수정

--- src/pages/product/AddProduct.jsx
+++ src/pages/product/AddProduct.jsx
@@ -1,19 +1,11 @@
-import Calendar from './addproduct/Calendar';
 import ProductImage from './addproduct/ProductImage';
-import ProductContent from './addproduct/ProductContent';
-import ProductSelectSpot from './addproduct/ProductSelectSpot';
-import Header from '@components/layout/Header';
 
 import '@components/style/layout.css';
 
 function AddProduct() {
   return (
-    <div className="layout scrollbar">
-      <Header />
+    <div>
       <ProductImage />
-      <Calendar />
-      <ProductSelectSpot />
-      <ProductContent />
     </div>
   );
 }
