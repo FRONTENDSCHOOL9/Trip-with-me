64c3b855475e4e5e16c5f2245ec2874316a90cd1
YANGJUNHO <yangwork96@gmail.com>
2024-04-17 16:02:50 +0900

    Docs: IntroHeader 컴포넌트 생성

---
+++ src/pages/intro/IntroHeader.jsx
@@ -0,0 +1,21 @@
+function IntroHeader() {
+  return (
+    <div>
+      <div className="flex mt-6 mb-24 justify-between px-6">
+        <div className="flex flex-col">
+          <img
+            className="-mb-1.5 ml-auto w-8 h-6"
+            src="/src/assets/icons/icon-logo.svg"
+            alt="메인 비행기 로고"
+          />
+          <h1 className="font-['SokchoBadaDotum'] font-bold text-xl text-center text-main-color">
+            Trip with me
+          </h1>
+        </div>
+        <button className="-mb-4">Skip</button>
+      </div>
+    </div>
+  );
+}
+
+export default IntroHeader;
