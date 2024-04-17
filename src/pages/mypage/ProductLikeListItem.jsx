46fbccf56278fc19048fa3428a6d6179b6d9ab62
chaeyoungg <chaeyoungg77@gmail.com>
2024-04-17 17:40:37 +0900

    Feat: 좋아요 기능 추가 및 디자인

--- src/pages/mypage/ProductLikeListItem.jsx
+++ src/pages/mypage/ProductLikeListItem.jsx
@@ -1,5 +1,6 @@
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 ProductLikeListItem.propTypes = {
@@ -8,50 +9,76 @@ ProductLikeListItem.propTypes = {
 
 function ProductLikeListItem({ item }) {
   const axios = useCustomAxios();
+  const [likeState, setLikeState] = useState(false);
 
   console.log('item=>', item);
-  item?.product?.name;
-  item?.product?.extra?.themes;
+  const won = item?.product?.price
+    .toString()
+    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 
   const handleLike = async e => {
-    try {
-      const res = await axios.delete(`/bookmarks/${item?._id}`);
-      console.log('item._id =>', item?._id);
-      console.log('res =>', res);
-      e.target.src = '/src/assets/icons/icon-heart-disabled.svg';
-    } catch (err) {
-      console.log(err);
+    setLikeState(prevState => !prevState);
+    //좋아요를 제거한 경우
+    if (likeState === false) {
+      try {
+        await axios.delete(`/bookmarks/${item?._id}`);
+        e.target.src = '/src/assets/icons/icon-heart-disabled.svg';
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    //다시 좋아요를 누른 경우
+    else if (likeState === true) {
+      try {
+        const res = await axios.post(
+          `/bookmarks/product/${item?.product?._id}`,
+          {},
+        );
+        console.log('item._id =>', item?._id);
+        console.log('res =>', res);
+        e.target.src = '/src/assets/icons/icon-heart-full.svg';
+      } catch (err) {
+        console.log(err);
+      }
     }
   };
-
   return (
-    <div className="py-2 px-5">
-      <Link to={`/product/${item?.product?._id}`}>
-        <img
-          className="min-h-36 max-w-sm bg-gray-200 rounded-xl mb-1"
-          src={`${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${item?.product?.image?.name}`}
-        />
-      </Link>
-      <div className="flex gap-5">
+    <div className="py-2 px-5 max-w-sm w-full mx-auto">
+      <div className=" max-w-80 w-full mx-auto">
         <Link to={`/product/${item?.product?._id}`}>
-          <p className="mb-1">{item?.product?.name}</p>
+          <div
+            className="bg-cover h-48 max-w-80 w-full mx-auto rounded-xl mb-1"
+            style={{
+              backgroundImage: `url(${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${item?.product?.image?.name})`,
+            }}
+          ></div>
+          {/* <img
+          className="max-h-36 h-full rounded-xl mb-1"
+          src={`${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${item?.product?.image?.name}`}
+        /> */}
         </Link>
-        <img
-          onClick={handleLike}
-          className="w-6 h-6 ml-auto cursor-pointer"
-          src="/src/assets/icons/icon-heart-full.svg"
-          alt=""
-        />
-      </div>
-      <div className="flex">
-        {item?.product?.extra?.themes?.map(item => (
-          <p
-            className="p-0.5 mx-1 mb-2 border-2 bg-second-color rounded-md"
-            key={item.id}
-          >
-            {item.name}
-          </p>
-        ))}
+        <div className="flex gap-5">
+          <Link to={`/product/${item?.product?._id}`}>
+            <p className="mb-1">{item?.product?.name}</p>
+          </Link>
+          <p className="mb-1 ml-auto font-bold">{won}원</p>
+        </div>
+        <div className="flex items-center">
+          {item?.product?.extra?.themes?.map(item => (
+            <p
+              className="p-0.5 mr-1 my-auto border-2 text-xs bg-second-color rounded "
+              key={item.id}
+            >
+              {item.name}
+            </p>
+          ))}
+          <img
+            onClick={handleLike}
+            className="w-6 h-6 ml-auto mr-2 cursor-pointer "
+            src="/src/assets/icons/icon-heart-full.svg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
