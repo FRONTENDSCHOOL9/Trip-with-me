0c5cb85ffc293da716b46ecac785e1b56a755297
YANGJUNHO <yangwork96@gmail.com>
2024-04-17 09:26:40 +0900

    Fix: 안내문구 버그 기능 수정

--- src/pages/product/addproduct/ProductContent.jsx
+++ src/pages/product/addproduct/ProductContent.jsx
@@ -1,40 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const ProductContent = ({ setProductInfo }) => {
-  const [showUploadContent, setShowUploadPrompt] = useState(false);
+  const [showUploadContent, setShowUploadContent] = useState(false);
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때마다 content 값이 변경되면서 안내 메시지를 숨깁니다.
+    if (content.trim()) {
+      setShowUploadContent(false);
+    }
+  }, [content]);
 
   const handleContentChange = e => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const content = formData.get('content');
+    setContent(e.target.value);
+  };
 
+  const handleSubmit = e => {
+    e.preventDefault();
     if (!content.trim()) {
-      setShowUploadPrompt(true);
+      setShowUploadContent(true);
       return;
     }
-
     setProductInfo(prevInfo => ({ ...prevInfo, content: content }));
   };
 
   return (
     <div>
       <form
-        onSubmit={handleContentChange}
+        onSubmit={handleSubmit}
         className="flex flex-col justify-center items-center"
       >
         <label htmlFor="content">
           <p className="text-2xl font-notosans font-semibold text-main-color mt-20">
             여행 상세 정보를 입력해주세요
           </p>
-          <p className="text-sm text-center font-notosans  text-main-color mt-4 mb-10">
+          <p className="text-sm text-center font-notosans text-main-color mt-4 mb-10">
             예시) 차량,숙박,제공등
           </p>
         </label>
         <textarea
           id="content"
           name="content"
-          className="relative w-80 h-96 bg-light-gray  rounded-lg"
+          className="relative w-80 h-96 bg-light-gray rounded-lg"
           placeholder="상세 정보를 입력해주세요"
+          value={content}
+          onChange={handleContentChange}
         ></textarea>
         {showUploadContent && (
           <p className="text-warning-color font-notosans font-medium text-sm">
