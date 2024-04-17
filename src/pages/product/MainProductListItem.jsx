2c7ce72d0b54a8faf4b8483fe4d809baba4f462a
D0-HA <withchemi970@gmail.com>
2024-04-17 10:41:32 +0900

    Feat: 판매 페이지 API 통신 및 상품 품절시 스타일 추가
    - 판매 페이지에서 API 상품 정보 렌더링
    - 메인 페이지, 판매 페이지에서 인원 모집 마감시 가상요소로 스타일 추가,
      가상요소 클릭시에도 해당 상품 상세정보로 이동 가능

--- src/pages/product/MainProductListItem.jsx
+++ src/pages/product/MainProductListItem.jsx
@@ -1,46 +1,73 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 const MainProductListItem = ({ item }) => {
-  console.log('item', item);
+  const isSoldOut = item.buyQuantity >= item.quantity;
+
   return (
-    <li className="productBox">
-      <div className="productBox-img">
-        <Link to={`/products/${item.id}`}>
-          <img
-            src={`${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${item?.mainImages[0]?.name}`}
-            className="img-item"
-          ></img>
-        </Link>
-      </div>
+    <li className="h-[330px] p-2">
+      <div
+        className={classNames({
+          relative: isSoldOut,
+        })}
+      >
+        {isSoldOut && (
+          <Link
+            to={`/products/${item.id}`}
+            className="absolute inset-0 flex justify-center items-center"
+            style={{ zIndex: 1 }}
+          >
+            <div className="bg-black bg-opacity-45 w-full h-full flex justify-center items-center rounded-lg">
+              <p className="text-zinc-200 text-3xl font-bold">
+                모집 마감되었어요.
+              </p>
+            </div>
+          </Link>
+        )}
+        <div>
+          <div className="w-102.5 h-56 overflow-hidden rounded-[10px]">
+            <Link to={`/products/${item.id}`}>
+              <img
+                src={`${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${item?.mainImages[0]?.name}`}
+              />
+            </Link>
+          </div>
 
-      <div className="productBox-info">
-        <Link to={`/products/${item.id}`}>
-          <h3 className="productTitle">{item?.name}</h3>
-        </Link>
-        <div className="productBox-limit">
-          <img
-            src="../src/assets/icons/icon-group.svg"
-            className="img-limit"
-          ></img>
-          <p className="limit">
-            {item?.buyQuantity}/{item?.quantity}
-          </p>
-        </div>
+          <div className="p-2 relative">
+            <Link to={`/products/${item.id}`}>
+              <h3 className="text-base font-medium">{item.name}</h3>
+            </Link>
+            <div className="flex text-sm">
+              <img
+                src="../src/assets/icons/icon-group.svg"
+                className="w-4 mr-1"
+              />
+              <p>{`${item.buyQuantity}/${item.quantity}`}</p>
+            </div>
 
-        <div className="productBox-etc">
-          {item?.extra?.themes?.map(item => {
-            <a href="#" className="tag" key={item.id}>
-              # {item.name}
-            </a>;
-          })}
-          <button type="button">
-            <img
-              src="../src/assets/icons/icon-heart-disabled.svg"
-              className="img-like"
-            ></img>
-          </button>
-          <p>2024</p>
+            <div className="mt-1">
+              {item.extra?.themes?.map(theme => (
+                <a
+                  href="#"
+                  className="bg-indigo-100 rounded mr-1 pb-0.5 px-0.5 text-sm font-medium"
+                  key={theme.id}
+                >
+                  # {theme.name}
+                </a>
+              ))}
+            </div>
+
+            <div className="absolute top-5 right-7 text-center">
+              <button type="button">
+                <img
+                  src="../src/assets/icons/icon-heart-disabled.svg"
+                  className="w-8"
+                />
+              </button>
+              <p className="-mt-2">{item?.bookmarks}</p>
+            </div>
+          </div>
         </div>
       </div>
     </li>
