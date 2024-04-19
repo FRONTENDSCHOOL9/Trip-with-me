48d1929e76c79dc35bbf5f62ec925b226c0bdd50
chaeyoungg <chaeyoungg77@gmail.com>
2024-04-19 11:42:09 +0900

    Feat : 메인페이지 좋아요 기능 추가, 경로 수정

--- src/pages/product/MainProductListItem.jsx
+++ src/pages/product/MainProductListItem.jsx
@@ -1,9 +1,49 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import { useState } from 'react';
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
 
 const MainProductListItem = ({ item }) => {
   const isSoldOut = item.buyQuantity >= item.quantity;
+  const [likeState, setLikeState] = useState(true);
+  const [likeId, setLikeId] = useState(item?._id);
+  const axios = useCustomAxios();
+
+  console.log('item', item, item?._id);
+
+  const handleLike = async (e) =>{
+    setLikeState(prevState => !prevState);
+
+    if(likeState === false){
+      try {
+        const res = await axios.delete(`/bookmarks/${likeId}`);
+        e.target.src = '/src/assets/icons/icon-heart-disabled.svg';
+        console.log(res);
+        console.log('좋아요 제거한 경우 item._id =>', likeId);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    else if (likeState === true) {
+      try {
+        //좋아요 누를 때에는 상품 id를 보낸다.
+        const res = await axios.post(
+          `/bookmarks/product/${item?._id}`,
+          {},
+        );
+        setLikeId(res?.data?.item?._id);
+        console.log(
+          '좋아요 누른 경우 북마크 id 새로 세팅 =>',
+          res?.data?.item?._id,
+        );
+        // console.log('res =>', res);
+        e.target.src = '/src/assets/icons/icon-heart-full.svg';
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
 
   return (
     <li className="h-82.5 p-2 mb-1 font-notosans">
@@ -14,7 +54,7 @@ const MainProductListItem = ({ item }) => {
       >
         {isSoldOut && (
           <Link
-            to={`/products/${item._id}`}
+            to={`/product/${item._id}`}
             className="absolute inset-0 flex justify-center items-center"
             style={{ zIndex: 1 }}
           >
@@ -27,7 +67,7 @@ const MainProductListItem = ({ item }) => {
         )}
         <div>
           <div className="w-102.5 h-56 overflow-hidden rounded-[10px]">
-            <Link to={`/products/${item.id}`}>
+            <Link to={`/product/${item._id}`}>
               <img
                 className="size-full object-cover"
                 src={`${import.meta.env.VITE_API_SERVER}/files/01-Trip-with-me/${item?.mainImages[0]?.name}`}
@@ -36,7 +76,7 @@ const MainProductListItem = ({ item }) => {
           </div>
 
           <div className="p-2 relative">
-            <Link to={`/products/${item.id}`}>
+            <Link to={`/product/${item._id}`}>
               <h3 className="text-base font-medium max-w-70 flex-grow overflow-hidden text-ellipsis whitespace-nowrap">
                 {item?.name}
               </h3>
@@ -64,7 +104,8 @@ const MainProductListItem = ({ item }) => {
             <div className="absolute top-8 right-7 text-center">
               <button type="button">
                 <img
-                  src="../src/assets/icons/icon-heart-disabled.svg"
+                  onClick={handleLike}
+                  src="/src/assets/icons/icon-heart-disabled.svg"
                   className="w-8"
                 />
               </button>
