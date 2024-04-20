c0bc36b3bacd6a7ce04517a085deb9a4f089ffe7
D0-HA <withchemi970@gmail.com>
2024-04-21 00:39:50 +0900

    Docs: route에 검색창 경로 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -22,6 +22,7 @@ import ProductContent from '@pages/product/addproduct/ProductContent';
 import ProductAddModal from '@pages/product/addproduct/ProductAddModal';
 import MyPageOther from '@pages/mypage/MyPageOther';
 import Intro from '@pages/intro/Intro';
+import SearchPage from '@pages/product/SearchPage';
 
 const router = createBrowserRouter([
   {
@@ -128,6 +129,10 @@ const router = createBrowserRouter([
   {
     path: '/trip-with-me/intro',
     element: <Intro />,
+  },
+  {
+    path: '/search',
+    element: <SearchPage />,
   },
 ]);
 
