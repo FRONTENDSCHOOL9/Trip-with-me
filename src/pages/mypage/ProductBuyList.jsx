8f0afcc2a2766dbeeb74b42650aa0612b9821dd3
chaeyoungg <chaeyoungg77@gmail.com>
2024-04-21 18:01:39 +0900

    Fix: 구매목록 sorting 추가

--- src/pages/mypage/ProductBuyList.jsx
+++ src/pages/mypage/ProductBuyList.jsx
@@ -8,62 +8,61 @@ function ProductBuyList() {
   const setPageName = usePageStore(state => state.setPageName);
   const axios = useCustomAxios();
   const [pageParam, setPageParam] = useState(1);
-  const [itemList, setItemList]= useState([]);
+  const [itemList, setItemList] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
 
-  const getBuyList = async() => {
-    try{
+  const getBuyList = async () => {
+    try {
       const res = await axios.get('/orders', {
-        params : {
-          page : pageParam,
-          limit : 3,
-        }
-      })
-      const list = res?.data?.item?.map((item)=>{
-        return <ProductBuyListItem key={item?._id} item={item} />
-      })
+        params: {
+          sort: `{ "products.extra.date.endDate": -1 }`,
+          page: pageParam,
+          limit: 3,
+        },
+      });
+      const list = res?.data?.item?.map(item => {
+        return <ProductBuyListItem key={item?._id} item={item} />;
+      });
       let newItemList = [...itemList, ...list];
-      console.log('newItemList',newItemList);
+      console.log('newItemList', newItemList);
       // console.log('res', res);
       let endPage = res?.data?.pagination?.totalPages;
       let nowPage = res?.data?.pagination?.page;
       console.log(endPage);
       setTotalPages(endPage);
       setItemList(newItemList);
-      setPageParam(nowPage+1);
-
-    }
-    catch(err){
+      setPageParam(nowPage + 1);
+    } catch (err) {
       console.log(err.message);
     }
-  }
+  };
 
-  const handleClick = (e) => {
-    if(pageParam < totalPages){
+  const handleClick = e => {
+    if (pageParam < totalPages) {
       getBuyList();
-    }else if(pageParam == totalPages){
+    } else if (pageParam == totalPages) {
       getBuyList();
       e.target.className = 'hidden';
     }
-  }
+  };
 
-  console.log('list',itemList);
-
+  console.log('list', itemList);
 
   useEffect(() => {
     getBuyList();
     setPageName(page);
   }, []);
 
-  
   return (
     <div className="mb-8 flex flex-col">
-    {itemList}
-    <button className='mx-auto border border-main-color rounded-lg text-sm text-white tracking-widest' onClick={handleClick}>
-      <img className="w-8" src="/src/assets/icons/icon-more.svg" alt="" />
-    </button>
+      {itemList}
+      <button
+        className="mx-auto border border-main-color rounded-lg text-sm text-white tracking-widest"
+        onClick={handleClick}
+      >
+        <img className="w-8" src="/src/assets/icons/icon-more.svg" alt="" />
+      </button>
     </div>
-
   );
 }
 
