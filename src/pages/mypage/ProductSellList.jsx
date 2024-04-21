4fd206fe359fd3765ba625b00a2623cc2b0b4e46
D0-HA <withchemi970@gmail.com>
2024-04-21 22:19:43 +0900

    Docs: 판매 상품 목록 조회 정보 수정 및 불필요한 코드 삭제

--- src/pages/mypage/ProductSellList.jsx
+++ src/pages/mypage/ProductSellList.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react';
 import usePageStore from '@zustand/pageName.mjs';
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import ProductSellListItem from '@pages/mypage/ProductSellListItem';
-// import InfiniteScroll from 'react-infinite-scroller';
-// import { useInfiniteQuery } from '@tanstack/react-query';
 import { BeatLoader } from 'react-spinners';
 
 function ProductSellList() {
@@ -12,53 +10,7 @@ function ProductSellList() {
   const axios = useCustomAxios();
   const [sellData, setSellData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  // const { data, fetchNextPage } = useInfiniteQuery({
-  //   queryKey: ['/seller/products'],
-  //   queryFn: ({ pageParam = 1 }) =>
-  //     axios.get(`/seller/products`, {
-  //       params: {
-  //         page: pageParam,
-  //         limit: 3,
-  //       },
-  //     }),
 
-  //   getNextPageParam: (lastPage, allPages) => {
-  //     console.log('lastPage', lastPage);
-  //     const totalPages = lastPage.data.pagination.totalPages;
-  //     let nextPage =
-  //       allPages.length < totalPages ? allPages.length + 1 : undefined;
-  //     return nextPage;
-  //   },
-  // });
-
-  // // latestProcudts = renderProductItems = productList
-  // const latestProcudts = data?.pages?.flatMap(page =>
-  //   page.data.item.sort((a, b) => {
-  //     const dateA = new Date(a.createdAt);
-  //     const dateB = new Date(b.createdAt);
-  //     return dateB - dateA;
-  //   }),
-  // );
-
-  // const renderProductItems = items => {
-  //   return items.map(item => (
-  //     <ProductSellListItem key={item?._id} item={item} />
-  //   ));
-  // };
-
-  // useEffect(() => {
-  //   if (data) {
-  //     const productList = renderProductItems(latestProcudts);
-  //     setSellData(productList);
-  //   }
-  //   setPageName(page);
-  // }, [data]);
-
-  // const hasNext =
-  //   data?.pages.at(-1).data.pagination.page <
-  //   data?.pages.at(-1).data.pagination.totalPages;
-
-  // 이전
   const getSellProduct = async () => {
     setIsLoading(true);
     try {
@@ -82,23 +34,13 @@ function ProductSellList() {
   }, []);
   return (
     <div>
-      <ul>
-        {/* <InfiniteScroll
-          pageStart={1}
-          loadMore={fetchNextPage}
-          hasMore={hasNext}
-        >
-          {sellData}
-        </InfiniteScroll> */}
-
-        {isLoading ? (
-          <div className="flex justify-center items-center h-[780px]">
-            <BeatLoader color="#68A9ED" />
-          </div>
-        ) : (
-          <li>{sellData}</li>
-        )}
-      </ul>
+      {isLoading ? (
+        <div className="flex justify-center items-center h-[780px]">
+          <BeatLoader color="#68A9ED" />
+        </div>
+      ) : (
+        <ul>{sellData}</ul>
+      )}
     </div>
   );
 }
