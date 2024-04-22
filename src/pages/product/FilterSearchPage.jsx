41ed593c52110c0a631038d659405b453dc12b17
D0HA <withchemi970@gmail.com>
2024-04-22 16:28:53 +0900

    Feat: 상품 검색 필터 기능 완료

--- src/pages/product/FilterSearchPage.jsx
+++ src/pages/product/FilterSearchPage.jsx
@@ -2,16 +2,30 @@ import Search from '@components/Search';
 import Title from '@components/Title';
 import Footer from '@components/layout/Footer';
 import MainProductItem from '@pages/product/MainProductItem';
+import { useLocation } from 'react-router-dom';
 
 const FilterSearchPage = () => {
+  const result = useLocation();
+  const list = result?.state?.item;
+
+  console.log('result.state', result.state);
+
   return (
     <div className="layout">
       <Title />
       <Search />
       <div className="scrollbar p-4">
-        <ul>
-          <MainProductItem key={item._id} item={item} />
-        </ul>
+        {list && list.length > 0 ? (
+          <ul>
+            {list.map(item => (
+              <MainProductItem key={item._id} item={item} />
+            ))}
+          </ul>
+        ) : (
+          <div className="h-80 p-2 flex items-center justify-center">
+            <p className="text-lg">찾는 상품이 없어요😢</p>
+          </div>
+        )}
       </div>
       <Footer />
     </div>
