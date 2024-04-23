0f9b9b924043e5c18b137ab3702167f63c73f847
YANGJUNHO <yangwork96@gmail.com>
2024-04-23 11:41:13 +0900

    Design: mycard padding 픽셀 수정

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -46,9 +46,9 @@ function MyPage() {
             </h2>
             <div className="profile-box mt-4 mx-6 flex h-64 mb-4 shadow-xl relative ">
               <h3 className="absolute top-4 left-4 font-bold">My Trip Card</h3>
-              <div className="flex flex-col w-[96px] mt-auto mb-6 pl-6">
+              <div className="flex flex-col w-[96px] mt-auto mb-6 pl-2">
                 <div>
-                  <p className="font-semibold text-lg text-white ">
+                  <p className="font-semibold text-xl text-white ">
                     {user?.name}
                   </p>
                   <p className="text-gray-700 font-light text-sm">
